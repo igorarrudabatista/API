7a75efd8daaf8a8b10f84d9bbd9604de07bc9e7e
Igor de Arruda Batista <00054957176@seduc.mt.gov.br>
2022-03-14 17:37:57 -0400

    swagger two

--- app/Http/Controllers/api/Seduc_Professor.php
+++ app/Http/Controllers/api/Seduc_Professor.php
@@ -15,7 +15,8 @@ class Seduc_Professor extends Controller
      * @return \Illuminate\Http\Response
      */
   
-/** @OA\Info(title="SEDUC API", version="0.1") 
+/** 
+ * @OA\Info(title="SEDUC API", version="0.1") 
 *
  * @OA\Get(
  *     path="/api/seduc/professor",
@@ -27,7 +28,7 @@ class Seduc_Professor extends Controller
  *  *     @OA\Parameter(
      *         name="ESCOLA_ID",
      *         in="query",
-     *         description="Listar todos os itens Tabela Professores",
+     *         description="ID Escola ",
      *         required=false,
      * 
      *         @OA\Schema(
@@ -40,7 +41,7 @@ class Seduc_Professor extends Controller
      * *     @OA\Parameter(
      *         name="ESCOLA_INEP",
      *         in="query",
-     *         description="Listar todos os itens Tabela Professores",
+     *         description="ID Escola Inep",
      *         required=false,
      * 
      *         @OA\Schema(
@@ -50,35 +51,158 @@ class Seduc_Professor extends Controller
      *          example="51034077",
      *         style="form"),
      * 
-     * 
-     * 
- *     @OA\Response(response="200", description="OK"),
- *     @OA\Response(response=422, description="Missing Data"),
- *     @OA\Response(response=401, description="fail"),
- *     @OA\Response(response=404, description="fail"),
- * )
- * 
- * 
- *  * @OA\Get(
- *     path="/api/seduc/alunos",
- *     tags={"Alunos"},
- *  *     summary="Get all Professor",
- *     description="Fetches all the Asset records",
-
- *  *     @OA\Parameter(
-     *         name="ESCOLA_ID",
+     * @OA\Parameter(
+     *         name="TURMA_GID",
      *         in="query",
-     *         description="Listar todos os itens Tabela Professores",
+     *         description="ID Escola Inep",
      *         required=false,
      * 
      *         @OA\Schema(
      *             type="array",
      *             @OA\Items(type="string"),
      *         ),
-     *          example="10766",
+     *          example="463476534675",
      *         style="form"),
+     * 
+     *    * @OA\Parameter(
+     *         name="TURMA_NAME",
+     *         in="query",
+     *         description="ID Escola Inep",
+     *         required=false,
+     * 
+     *         @OA\Schema(
+     *             type="array",
+     *             @OA\Items(type="string"),
+     *         ),
+     *          example="1\u00ba - 2\u00ba Ano B - EM Not",
+     *         style="form"),
+     * 
+     *         * @OA\Parameter(
+     *         name="TURMA_SURNAME",
+     *         in="query",
+     *         description="ID Escola Inep",
+     *         required=false,
+     * 
+     *         @OA\Schema(
+     *             type="array",
+     *             @OA\Items(type="string"),
+     *         ),
+     *          example="EEPA ACO - 2022",
+     *         style="form"),
+     * 
+     * 
+     *         * @OA\Parameter(
+     *         name="PROFESSOR_GID",
+     *         in="query",
+     *         description="ID Escola Inep",
+     *         required=false,
+     * 
+     *         @OA\Schema(
+     *             type="array",
+     *             @OA\Items(type="string"),
+     *         ),
+     *          example="114806048526598013023",
+     *         style="form"),
+     * 
+     *              * @OA\Parameter(
+     *         name="OWNER_EMAIL_GID",
+     *         in="query",
+     *         description="ID Escola Inep",
+     *         required=false,
+     * 
+     *         @OA\Schema(
+     *             type="array",
+     *             @OA\Items(type="string"),
+     *         ),
+     *          example="escola.10766@edu.mt.gov.br",
+     *         style="form"),
+     * 
+     *         @OA\Parameter(
+     *         name="NivelTurma",
+     *         in="query",
+     *         description="ID Escola Inep",
+     *         required=false,
+     * 
+     *         @OA\Schema(
+     *             type="array",
+     *             @OA\Items(type="string"),
+     *         ),
+     *          example="10,12",
+     *         style="form"),
+     *        @OA\Parameter(
+     *         name="CodigoIbgeMunicipio",
+     *         in="query",
+     *         description="ID Escola Inep",
+     *         required=false,
+     * 
+     *         @OA\Schema(
+     *             type="array",
+     *             @OA\Items(type="string"),
+     *         ),
+     *          example="5100102",
+     *         style="form"),
+     * 
+     *       @OA\Parameter(
+     *         name="NomeMunicipio",
+     *         in="query",
+     *         description="ID Escola Inep",
+     *         required=false,
+     * 
+     *         @OA\Schema(
+     *             type="array",
+     *             @OA\Items(type="string"),
+     *         ),
+     *          example="ACORIZAL",
+     *         style="form"),
+     * 
+     *  *       @OA\Parameter(
+     *         name="CodigoIbgeDRE",
+     *         in="query",
+     *         description="ID Escola Inep",
+     *         required=false,
+     * 
+     *         @OA\Schema(
+     *             type="array",
+     *             @OA\Items(type="string"),
+     *         ),
+     *          example="5108402",
+     *         style="form"),
+     * 
+     * *       @OA\Parameter(
+     *         name="NomeDRE",
+     *         in="query",
+     *         description="ID Escola Inep",
+     *         required=false,
+     * 
+     *         @OA\Schema(
+     *             type="array",
+     *             @OA\Items(type="string"),
+     *         ),
+     *          example="DRE VARZEA GRANDE",
+     *         style="form"),
+     * 
+     *  * *       @OA\Parameter(
+     *         name="NomeEscola",
+     *         in="query",
+     *         description="ID Escola Inep",
+     *         required=false,
+     * 
+     *         @OA\Schema(
+     *             type="array",
+     *             @OA\Items(type="string"),
+     *         ),
+     *          example="EE Ponce de Arruda",
+     *         style="form"),
+     * 
+     *  
+
+ *     @OA\Response(response="200", description="OK"),
+ *     @OA\Response(response=422, description="Missing Data"),
+ *     @OA\Response(response=401, description="fail"),
+ *     @OA\Response(response=404, description="fail"),
  * 
- *
+ * 
+ * )
  */
 
 
@@ -91,12 +215,57 @@ class Seduc_Professor extends Controller
        
        $consulta = EducarTech::get()->toJson(JSON_PRETTY_PRINT);
        
-       
-       
-    
        return response($consulta, 200); 
     }
 
+
+
+    /** 
+*
+ * @OA\Get(
+ *     path="/api/seduc/alunos",
+ *     tags={"Alunos"},
+ * 
+ *     summary="Get all Professor",
+ *     description="Fetches all the Asset records",
+
+ *  *     @OA\Parameter(
+     *         name="ESCOLA_ID",
+     *         in="query",
+     *         description="ID Escola ",
+     *         required=false,
+     * 
+     *         @OA\Schema(
+     *             type="array",
+     *             @OA\Items(type="string"),
+     *         ),
+     *          example="10766",
+     *         style="form"),
+     * 
+     * *     @OA\Parameter(
+     *         name="ESCOLA_INEP",
+     *         in="query",
+     *         description="ID Escola Inep",
+     *         required=false,
+     * 
+     *         @OA\Schema(
+     *             type="array",
+     *             @OA\Items(type="string"),
+     *         ),
+     *          example="51034077",
+     *         style="form"),
+     * 
+     * 
+     *  
+
+ *     @OA\Response(response="200", description="OK"),
+ *     @OA\Response(response=422, description="Missing Data"),
+ *     @OA\Response(response=401, description="fail"),
+ *     @OA\Response(response=404, description="fail"),
+ * 
+ * 
+ * )
+ */
     public function create()
     {
         //
